2e1ca4b4b6ef513772ecff9b9985bea261b9ab9f
rgn2698-wq <rgn2698@gmail.com>
2026-01-22 17:31:45 +0900

    feat : 20260122 location / variable / object / oprator

---
+++ ch01_js_basic/01_js_location.js
@@ -0,0 +1,2 @@
+console.log('Hello! javaScript!')
+document.write('<h1>별개의 .js파일에 작성 <br></h1>')
---
+++ ch01_js_basic/02_variable.js
@@ -0,0 +1,18 @@
+var x = 5
+var y = 6
+var z = x + y
+console.log(z)
+
+var x = 7;
+z = x + y
+console.log(z)
+
+console.log(a)    // 오류가 안뜸
+var a = '안녕하세요'
+console.log(a)
+
+/*
+12번 라인에서 선언되지도 않은 a라는 변수를 console에 찍을것을 요구 헀음에도 오류 로그가 되지않고 a 변수의 저장된 값이 undefined라고만 이야기했음.
+*/
+
+// 03_let_variable.js
